43aec9f45e1e98ee71331f0ee285fbb952300c07
Alejandro Zapata <keybladeschosen1@hotmail.com>
2018-03-24 01:18:35 -0700

    finished hangman, wrote out topics on README

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # ACM Python Workshop - Sample Hangman Syllabus
 
 ### General Tutorial Session
+- Material source: http://cs.lmu.edu/~ray/notes/intropython/
+
+# Python
+
+- is a scripting language: expressive and powerful, dynamically typed, garbage-collected, introspectable
+- has great, simple syntax for high-level types: tuples, lists, dictionaries, sets, named tuples
+- has cool stuff like exceptions, generators, list comprehensions, regexes
+- is object-oriented
+- has a module system to make large-scale programming possible
+- has a ton of useful libraries (including windowing, networking, system calls)
+- was created by Guido van Rossum, the BDFL, and first appeared as - Version 0.9.0 in 1991, reaching Version 1.0 in 1994
+- is used quite a lot at Google and NASA and thousands of other places
+
 #### What to cover:
+
+- Using Python from the terminal, compiling and running code.
+- Python REPL
+- Standard I/O.
+- Variables, functions, loops.
+  - range()
+- Variable types: integer, string, boolean, etc.
+- User interaction through the terminal.
+- Basic data structures: list, tuple, dictionary
+  - list comprehensions!
+  - generators
+- Classes
+ - attributes
+ - operator overloading
+- Inheritance and polymorphism
+- Importing/using modules
+ - API use
 
 ### Group Work - Creating Hangman
--- hangman.py
+++ hangman.py
@@ -1,33 +1,39 @@
-import sys
+import requests
 
-def main():
-    print("Welcome to Hangman")
-    if len(sys.argv) > 1:
-        createHangman(sys.argv[1])
+def get_guess():
+  dashes = "-" * len(secret_word)
+  guesses_left = 10
+
+  while guesses_left > 0 and not dashes == secret_word:
+    print(dashes)
+    print (str(guesses_left))
+
+    guess = input("Guess:")
+
+    if guess in secret_word:
+      print ("That letter is in the secret word!")
+      dashes = update_dashes(secret_word, dashes, guess)
     else:
-        createHangman(getWord())
+      print ("That letter is not in the secret word!")
+      guesses_left -= 1
 
-def getWord():
-    return "TEST"
+  if guesses_left == 0:
+    print ("You lose. The word was: " + str(secret_word))
+  else:
+    print ("Congrats! You win. The word was: " + str(secret_word))
 
-def createHangman(word):
-    guesses = 7
-    word = createWord("", word)
-    while guesses > 0:
-        print("You have ", guesses, "guesses remaining.")
-        print(displayWord)
-        letterGuess = input("Guess a letter ")
-        guesses = checkLetter(guesses, word, letterGuess)
+def update_dashes(secret, cur_dash, rec_guess):
+  result = ""
 
-def checkLetter(guesses, word, letter):
-    if word.find(letter) != -1 and letter != "":
-        print("Right!")
-        return guesses
+  for i in range(len(secret)):
+    if secret[i] == rec_guess:
+      result = result + rec_guess
     else:
-        print("Wrong!")
-        return guesses - 1
+      result = result + cur_dash[i]
 
-def winOrLoss():
-    return "TEST"
+  return result
 
-main()
+word_site = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+response = requests.get(word_site)
+secret_word = response.json()["word"]
+get_guess()
